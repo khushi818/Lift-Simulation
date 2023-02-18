7e919a159f1e1dc0ca82f126ac9321416ce6c1a7
khushi <khushishukla5890@gmail.com>
2023-02-18 06:16:41 +0530

    boundary defined

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,27 @@
 const val = document.getElementById('blocks')
-const height =val.offsetHeight;
-
 const root = document.querySelector(':root')
-root.style.setProperty('--block-height', height/3)
+const up = document.querySelector('.above')
+const down = document.querySelector('.below')
+
+
+const height =val.children[0].offsetHeight;
+console.log(height)
+
+up.addEventListener('click', ()=>{
+    let block_style = getComputedStyle(root,null);
+    let block = block_style.getPropertyValue('--block-height');
+    block = block.replace('px','')
+    console.log(parseInt(block) + (height/3))
+    root.style.setProperty('--block-height', `${parseInt(block) + height}px`) 
+})
+
+down.addEventListener('click', ()=>{
+    let block_style = getComputedStyle(root,null);
+    let block = block_style.getPropertyValue('--block-height');
+    block = block.replace('px','')
+    console.log(parseInt(block) + (height/3))
+    root.style.setProperty('--block-height', `${parseInt(block) - height}px`) 
+})
+
+
+
