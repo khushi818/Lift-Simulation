abf35a3564dca1369785e78a7c1cf1c724a4d93b
khushi <khushishukla5890@gmail.com>
2023-02-18 07:23:43 +0530

    state formation

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,45 @@
 const val = document.getElementById('blocks')
 const root = document.querySelector(':root')
-const up = document.querySelector('.above')
-const down = document.querySelector('.below')
+const up = document.querySelectorAll('.above')
+const down = document.querySelectorAll('.below')
 
-
+/*height of the seperate block*/
 const height =val.children[0].offsetHeight;
 console.log(height)
 
-up.addEventListener('click', ()=>{
-    let block_style = getComputedStyle(root,null);
-    let block = block_style.getPropertyValue('--block-height');
-    block = block.replace('px','')
-    console.log(parseInt(block) + (height/3))
-    root.style.setProperty('--block-height', `${parseInt(block) + height}px`) 
-})
+let previous_block = 0;
 
-down.addEventListener('click', ()=>{
+up.forEach(item => {
+  item.addEventListener('click', ()=>{
+     let total = 2-item.parentElement.dataset.column
+     let block_style = getComputedStyle(root,null);
+     let block = block_style.getPropertyValue('--block-height');// value of lift position
+     block = parseInt(block.replace('px',''))
+     console.log(parseInt(block) + (height))
+     
+     if((total * height) !== previous_block)
+     {
+      console.log(`total:${total} & block:${block} = ${total * height}`)
+      root.style.setProperty('--block-height', `${(total * height)}px`)
+      previous_block = block   
+     } 
+     else
+     {
+     root.style.setProperty('--block-height', `${block + height}px`)
+     } 
+ })
+});
+
+
+down.forEach(item => {
+ item.addEventListener('click', ()=>{
     let block_style = getComputedStyle(root,null);
     let block = block_style.getPropertyValue('--block-height');
     block = block.replace('px','')
     console.log(parseInt(block) + (height/3))
     root.style.setProperty('--block-height', `${parseInt(block) - height}px`) 
 })
+});
 
 
 
